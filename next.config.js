0a89dad0daa486de2c894858b108431f890f149b
takusan64 <hafe1119@yahoo.co.jp>
2021-10-13 19:16:16 +0900

    fixed base url

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
+const BASE_URL =
+  process.env.NODE_ENV === 'development' ? '' : '/react-practice'
+
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
+  assetPrefix: BASE_URL,
+  basePath: BASE_URL
 }
