46c3eeca986c1468143caa3bed15ea7e2445d7f2
takusan64 <hafe1119@yahoo.co.jp>
2021-10-15 00:27:11 +0900

    change import module, esmodule to common js

--- src/components/CodeHighlight.tsx
+++ src/components/CodeHighlight.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import {
   Container
 } from '@mui/material'
-import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter'
+import { dark } from 'react-syntax-highlighter/dist/cjs/styles/prism'
 
 type CodeHighlightProp = {
   code_str: string
