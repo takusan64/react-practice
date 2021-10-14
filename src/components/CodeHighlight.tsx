ca70c949f4b9a97ea622b018179748844891bd01
takusan64 <hafe1119@yahoo.co.jp>
2021-10-15 00:12:54 +0900

    reate code highlight components

---
+++ src/components/CodeHighlight.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import {
+  Container
+} from '@mui/material'
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+
+type CodeHighlightProp = {
+  code_str: string
+}
+
+const CodeHighlight: React.FC<CodeHighlightProp> = ({ code_str }) => {
+  const customCSS = {fontSize: "0.75em"}
+
+  return (
+    <>
+      <Container maxWidth="md">
+        <SyntaxHighlighter language="tsx" style={dark} customStyle={customCSS}>
+          {code_str}
+        </SyntaxHighlighter>
+      </Container>
+    </>
+  )
+}
+
+export default CodeHighlight
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,13 +1,46 @@
 import type { NextPage } from 'next'
 import Link from "next/link"
+import CodeHighlight from 'components/CodeHighlight'
 
-const About:NextPage = () =>{
-  return(
+const About: NextPage = () => {
+  const codeString = `import type { NextPage } from 'next'
+  import Link from 'next/link'
+  import { createStyles, makeStyles } from '@mui/styles';
+  import { Theme } from '@mui/material/styles';
+  import { grey } from '@mui/material/colors'
+
+  const useStyles = makeStyles((theme: Theme) =>
+    createStyles({
+      root: {
+        backgroundColor: grey[200]
+      },
+    }),
+  );
+
+  const Home: NextPage = () => {
+    const classes = useStyles()
+    return (
+      <div className={classes.root}>
+        <h1>
+          Welcome to <a href="https://nextjs.org">Next.js!</a>
+        </h1>
+        <h2>
+          <Link href="/about">
+            <button>Go about Page.</button>
+          </Link>
+        </h2>
+      </div>
+    )
+  }
+
+  export default Home`;
+  return (
     <>
       <p>About page</p>
       <Link href="/">
         <a> Go to Home </a>
       </Link>
+      <CodeHighlight code_str={codeString} />
     </>
   )
 }
