a098b5c565c0b4c5c56545c27f34eb42dde0d56b
takusan64 <hafe1119@yahoo.co.jp>
2021-10-20 23:40:46 +0900

    create useState Page

--- src/components/hooks/useState/UseStateExample.tsx
+++ src/components/hooks/useState/UseStateExample.tsx
@@ -1,16 +1,72 @@
 import React, { useState } from 'react'
 import CodeHighlight from 'components/CodeHighlight'
 import {
-  Container
+  IconButton,
+  Container,
+  Typography
 } from '@mui/material'
+import {
+  ArrowUpward,
+  ArrowDownward
+} from '@mui/icons-material'
 import { UseStateString } from './UseStateString'
 
-const UseStateExmample = () => {
+// Function Component
+const UseStateFunctionComponent: React.FC = () => {
+  const [count, setCount] = useState(0)
 
   return (
     <>
-      <p>useState</p>
-      <CodeHighlight code_str={UseStateString} />
+      <Typography variant="subtitle1" gutterBottom>
+        Function Compnent Counter = {count}
+        <IconButton onClick={() => setCount(count + 1)}>
+          <ArrowUpward />
+        </IconButton>
+        <IconButton onClick={() => setCount(count - 1)}>
+          <ArrowDownward />
+        </IconButton>
+      </Typography>
+    </>
+  )
+}
+
+// Class Component
+interface ClassState {
+  count: number
+}
+class UseStateClassComponent extends React.Component<{}, ClassState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+  render() {
+    return (
+      <>
+        <Typography variant="subtitle1" gutterBottom>
+          Class Component Counter = {this.state.count}
+          <IconButton onClick={() => this.setState({ count: this.state.count + 1 })}>
+            <ArrowUpward />
+          </IconButton>
+          <IconButton onClick={() => this.setState({ count: this.state.count - 1 })}>
+            <ArrowDownward />
+          </IconButton>
+        </Typography>
+      </>
+    )
+  }
+}
+
+const UseStateExmample: React.FC = () => {
+
+  return (
+    <>
+      <Container maxWidth="md">
+        <UseStateFunctionComponent />
+        <UseStateClassComponent />
+        <CodeHighlight code_str={UseStateString} />
+      </Container>
     </>
   )
 }
--- src/components/hooks/useState/UseStateString.ts
+++ src/components/hooks/useState/UseStateString.ts
@@ -1,1 +1,46 @@
-export const UseStateString:string = ``
+export const UseStateString:string = `// Function Component
+const UseStateFunctionComponent: React.FC = () => {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Typography variant="subtitle1" gutterBottom>
+        Function Compnent Counter = {count}
+        <IconButton onClick={() => setCount(count + 1)}>
+          <ArrowUpward />
+        </IconButton>
+        <IconButton onClick={() => setCount(count - 1)}>
+          <ArrowDownward />
+        </IconButton>
+      </Typography>
+    </>
+  )
+}
+
+// Class Component
+interface ClassState {
+  count: number
+}
+class UseStateClassComponent extends React.Component<{}, ClassState> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {
+      count: 0
+    }
+  }
+  render() {
+    return (
+      <>
+        <Typography variant="subtitle1" gutterBottom>
+          Class Component Counter = {this.state.count}
+          <IconButton onClick={() => this.setState({ count: this.state.count + 1 })}>
+            <ArrowUpward />
+          </IconButton>
+          <IconButton onClick={() => this.setState({ count: this.state.count - 1 })}>
+            <ArrowDownward />
+          </IconButton>
+        </Typography>
+      </>
+    )
+  }
+}`
